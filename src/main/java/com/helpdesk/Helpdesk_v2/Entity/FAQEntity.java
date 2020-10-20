7b88962153cba044c003fb400eb504c1690401a6
TanKy <pokijellyperiod1@gmail.com>
2020-10-20 08:55:31 +0700

    ádsa

--- src/main/java/com/helpdesk/Helpdesk_v2/Entity/FAQEntity.java
+++ src/main/java/com/helpdesk/Helpdesk_v2/Entity/FAQEntity.java
@@ -3,26 +3,28 @@ package com.helpdesk.Helpdesk_v2.Entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.helpdesk.Helpdesk_v2.Model.MultiLanguageObject;
+
 @Document(collection = "faq")
 public class FAQEntity {
 
 	@Id
 	private String id;
 
-	private String question;
-	private String answer;
+	private MultiLanguageObject question;
+	private MultiLanguageObject answer;
 
 	public FAQEntity() {
 		// TODO Auto-generated constructor stub
 	}
-
-	public FAQEntity(String question, String answer) {
+	
+	public FAQEntity(MultiLanguageObject question, MultiLanguageObject answer) {
 		super();
 		this.question = question;
 		this.answer = answer;
 	}
 
-	public FAQEntity(String id, String question, String answer) {
+	public FAQEntity(String id, MultiLanguageObject question, MultiLanguageObject answer) {
 		super();
 		this.id = id;
 		this.question = question;
@@ -37,20 +39,29 @@ public class FAQEntity {
 		this.id = id;
 	}
 
-	public String getQuestion() {
+	public MultiLanguageObject getQuestion() {
 		return question;
 	}
 
-	public void setQuestion(String question) {
+	public void setQuestion(MultiLanguageObject question) {
 		this.question = question;
 	}
 
-	public String getAnswer() {
+	public MultiLanguageObject getAnswer() {
 		return answer;
 	}
 
-	public void setAnswer(String answer) {
+	public void setAnswer(MultiLanguageObject answer) {
 		this.answer = answer;
 	}
+	
+	
+	
+
+	
+	
+	
+	
+	
 
 }
